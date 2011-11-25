6a73443947e804fec18a38dd3e323b64a489f14d
Mauro Krikorian <mauro.krikorian@southworks.net>
2011-11-25 15:12:52 -0300

    # table API tests updated/refactored

---
+++ library/storage.tests/src/com/windowsazure/samples/android/storageclient/tests/CloudTableClientUsingWAZServiceByHttpTests.java
@@ -0,0 +1,3 @@
+package com.windowsazure.samples.android.storageclient.tests;
+
+public class CloudTableClientUsingWAZServiceByHttpTests extends CloudTableClientTests<WAZServiceHttpAccountProvider> { }
---
+++ library/storage.tests/src/com/windowsazure/samples/android/storageclient/tests/CloudTableObjectUsingAccountAndKeyTests.java
@@ -0,0 +1,3 @@
+package com.windowsazure.samples.android.storageclient.tests;
+
+public class CloudTableObjectUsingAccountAndKeyTests extends CloudTableObjectTests<CloudStorageAccountProvider> { }
