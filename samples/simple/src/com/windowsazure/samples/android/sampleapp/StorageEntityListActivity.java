d02f47e8b016b007fe3ff4373b3902facd6a171c
dgeffner <diego.geffner@southworks.net>
2011-11-30 12:14:25 -0300

    We updated the ACS sample to use the proper ACS Namespace

--- samples/simple/src/com/windowsazure/samples/android/sampleapp/StorageEntityListActivity.java
+++ samples/simple/src/com/windowsazure/samples/android/sampleapp/StorageEntityListActivity.java
@@ -3,7 +3,6 @@ package com.windowsazure.samples.android.sampleapp;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Hashtable;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
