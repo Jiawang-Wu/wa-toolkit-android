bea9fe47220f7c933ff40c0a1203b8517c0c6035
dgeffner <diego.geffner@southworks.net>
2011-12-02 16:05:04 -0300

    Added the JavaDoc for CloudQueueClient.java, CloudQueue.java, CloudQueueMessage.java and QueueAttributes.java

--- library/storage/src/com/windowsazure/samples/android/storageclient/CloudQueueClient.java
+++ library/storage/src/com/windowsazure/samples/android/storageclient/CloudQueueClient.java
@@ -12,38 +12,91 @@ public class CloudQueueClient {
 	private URI m_BaseUri;
 	private StorageCredentials m_Credentials;
 
-	public CloudQueueClient(String baseAddress, StorageCredentials credentials) throws URISyntaxException {
-		this(new URI(baseAddress), credentials);
+	/**
+	Initializes a new instance of the CloudQueueClient class using the specified Queue service endpoint and account credentials.
+	@param endpoint The base URI used to construct the Queue service client.
+	@param credentials The account credentials used to create the Queue service client.
+	@see StorageCredentials 
+	*/
+	public CloudQueueClient(String endpoint, StorageCredentials credentials) throws URISyntaxException {
+		this(new URI(endpoint), credentials);
 	}
-	public CloudQueueClient(URI baseAddress, StorageCredentials credentials) throws URISyntaxException {
-		m_BaseUri = baseAddress;
+
+	/**
+	Initializes a new instance of the CloudQueueClient class using the specified Queue service endpoint and account credentials.
+	@param endpoint The base URI used to construct the Queue service client.
+	@param credentials The account credentials used to create the Queue service client.
+	@see StorageCredentials 
+	*/
+	public CloudQueueClient(URI endpoint, StorageCredentials credentials) throws URISyntaxException {
+		m_BaseUri = endpoint;
 		m_Credentials = credentials;
 	}
+
+	/**
+	Initializes a new instance of the CloudQueueClient class using the default Queue service endpoint and the specified account credentials.
+	@param credentials The account credentials used to create the Queue service client.
+	@see StorageCredentials 
+	*/
 	public CloudQueueClient(StorageCredentials credentials) throws URISyntaxException {
 		this(CloudStorageAccount.getDefaultQueueEndpoint(CloudStorageAccount.getDefaultScheme(),
 				credentials.getAccountName()), credentials);
 	}
+
+	/**
+	Gets the account credentials used to create the Queue service client.
+	@return StorageCredentials 
+	@see StorageCredentials 
+	*/
 	public StorageCredentials getCredentials() {
 		return m_Credentials;
 
 	}
 
+	/**
+	The base URI used to construct the Queue service client.
+	@return URI 
+	*/
 	public URI getBaseUri() {
 		return m_BaseUri;
 	}
 
+	/**
+	Gets a reference to the queue at the specified address.
+	@param queueAddress the address referencing the queue to be get
+	@return CloudQueue 
+	@see CloudQueue 
+	*/
 	public CloudQueue getQueueReference(String queueAddress) throws URISyntaxException {
 		return new CloudQueue(queueAddress, this);
 	}
 
+	/**
+	Returns an enumerable collection of the queues in the storage account.
+	@return Iterable<CloudQueue> 
+	@see CloudQueue 
+	*/
 	public Iterable<CloudQueue> listQueues() throws UnsupportedEncodingException, StorageException, IOException {
 		return this.listQueues(null);
 	}
 
+	/**
+	Returns an enumerable collection of the queues in the storage account whose names begin with the specified prefix.
+	@param prefix prefix that will be used to filter which queues are returned
+	@return Iterable<CloudQueue> 
+	@see CloudQueue 
+	*/
 	public Iterable<CloudQueue> listQueues(String prefix) throws UnsupportedEncodingException, StorageException, IOException {
 		return this.listQueues(prefix, QueueListingDetails.All);
 	}
 
+	/**
+	Returns an enumerable collection of the queues in the storage account whose names begin with the specified prefix.
+	@param prefix prefix that will be used to filter which queues are returned
+	@param detailsIncluded controls which details of the queues listed will be retrieved
+	@return Iterable<CloudQueue> 
+	@see CloudQueue 
+	*/
 	public Iterable<CloudQueue> listQueues(final String prefix,
 			final QueueListingDetails detailsIncluded) throws UnsupportedEncodingException, StorageException, IOException {
 		final CloudQueueClient client = this;
